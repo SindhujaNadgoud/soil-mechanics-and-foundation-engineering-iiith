9769db73d895159daf5b7a2f08f35f002f5660f8
Sindhuja <snadgoud99@gmail.com>
2019-08-28 23:47:49 +0530

    added most of the animations,stuck near getting pieces of a div

--- SRIP/Codes/basic.js
+++ SRIP/Codes/basic.js
@@ -1,10 +1,16 @@
 
 function myMove() {
+  setTimeout(cuprotate,200);
+  setTimeout(boxmove,9000);
+}
+
+function boxmove()
+{
   var elem = document.getElementById("recttop");   
   var pos = 0;
   var id = setInterval(frame, 1);
   function frame() {
-    if (pos == 210) {
+    if (pos == 280) {
       clearInterval(id);
     } else {
       pos++; 
@@ -13,3 +19,21 @@ function myMove() {
     }
   }
 }
+
+function cuprotate()
+{
+  document.getElementById("cup").style.transform = "rotate(-45deg)"; 
+   setTimeout(showdrop,600);
+   setTimeout(colourchane, 8000);
+}
+
+function colourchane()
+{
+    document.getElementById("cup").style.backgroundColor="rgba(0,0,0,0.0)";
+}
+ 
+ function showdrop()
+ {
+   document.getElementById('drop').style.display = "block";
+   document.getElementById('mudsample').style.height="145px";
+ }
